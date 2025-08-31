26f63723952a15c281001bbfc50ef61ea4a9779b
OnkarJondhale <onkar.jondhale@gmail.com>
2025-09-01 02:27:21 +0530

    Add new lines of code to trigger poll SCM

--- main.c
+++ main.c
@@ -3,5 +3,6 @@
 int main()
 {
     printf("This code is fetched from a github repository \n(adding a new line character) \n");
+    printf("This commit is for triggering the poll SCM \n");
     return 0;
 }
