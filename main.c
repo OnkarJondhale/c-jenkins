9eca8dade265b18affa226daea68329faa40bfd7
OnkarJondhale <onkar.jondhale@gmail.com>
2025-09-02 02:01:22 +0530

    Checking email notification

--- main.c
+++ main.c
@@ -4,5 +4,6 @@ int main()
 {
     printf("This code is fetched from a github repository \n(adding a new line character) \n");
     printf("This commit is for triggering the poll SCM \n");
+    printf("This line does not have semicolon to fail the jenkins build so that it can send the email notification about it")
     return 0;
 }
